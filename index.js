58435d2c6cb67c0db4a9884953768d06537169ef
Daniel H <frontend@reddesignsystems.com>
2016-05-10 18:57:37 -0500

    Request ajax all shows and render template

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(function() {
 	/**
 	 * Submit search form
 	 */
-	 $('#app-body')
+	$('#app-body')
 	 	.find('form')
 	 	.submit(function (ev) {
 	 		ev.preventDefault();
@@ -11,4 +11,31 @@ $(function() {
 	 			.val();
 	 		alert(query)
 		})
+
+	var template = '<article class="tv-show">' +
+					'<div class="left img-container">' +
+						'<img src=":img:" alt=":img alt:">' +
+					'</div>' +
+					'<div class="right info">' +
+						'<h1>:name:</h1>' +
+						'<p>:summary:</p>' +
+					'</div>' +
+				'</article>';
+
+	$.ajax({
+		url : 'http://api.tvmaze.com/shows',
+		success: function (shows, textStatus, xhr) {
+			shows.forEach(function (show) {
+				var article = template
+					.replace(':name:', show.name)
+					.replace(':img:', show.image.medium)
+					.replace(':summary:', show.summary)
+					.replace(':img alt:', show.name + "Logo")
+
+				$('#app-body')
+					.find('.tv-shows')
+					.append($(article))
+			})
+		}
+	})
 })
