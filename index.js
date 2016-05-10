100bbcce422fcfd091d8bd1af0269945af2f2572
Daniel H <frontend@reddesignsystems.com>
2016-05-10 17:59:29 -0500

    Add form input search

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 $(function() {
-	var header = $('header')
+	/**
+	 * Submit search form
+	 */
+	 $('#app-body')
+	 	.find('form')
+	 	.submit(function (ev) {
+	 		ev.preventDefault();
+	 		var query = $(this)
+	 			.find('input[type="text"]')
+	 			.val();
+	 		alert(query)
+		})
 })
