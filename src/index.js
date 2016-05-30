e79e42f5cb486e55b8e7f80321ec5ec6504b77cd
Daniel hernandez <danielhernandez16@hotmail.com>
2016-05-30 13:33:07 -0500

    Add funcionality like to tv-shows-container module

--- src/index.js
+++ src/index.js
@@ -35,17 +35,3 @@ page('/search', function (ctx, next) {
 })
 
 page()
-// $(function() {
-
-// 	
-
-// 	$tvShowsContainer.on('click', 'button.like', function (ev) {
-// 		var $this = $(this);
-// 		$this.animate({
-// 			'fontSize': '30px'
-// 		}, 'fast')
-// 		$this.closest('.tv-show')  /*closets function de jquery que busca padre que cumpla condicion*/
-// 			.toggleClass('liked')
-// 	})
- 	
-// })
--- src/tv-shows-container/index.js
+++ src/tv-shows-container/index.js
@@ -1,4 +1,14 @@
 import $ from 'jquery';
 
+var $tvShowsContainer = $('#app-body').find('.tv-shows');
 
-export default $('#app-body').find('.tv-shows');
+$tvShowsContainer.on('click', 'button.like', function (ev) {
+	var $this = $(this);
+	$this.animate({
+		'fontSize': '30px'
+	}, 'fast')
+	$this.closest('.tv-show')  /*closets function de jquery que busca padre que cumpla condicion*/
+		.toggleClass('liked')
+})
+
+export default $tvShowsContainer
