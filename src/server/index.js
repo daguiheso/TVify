506c1a78fcd90e03cb9bbcf817af319e86045c71
Daniel hernandez <danielhernandez16@hotmail.com>
2016-06-06 22:29:54 -0500

    Add express, endpoints and middleware static public

--- src/server/index.js
+++ src/server/index.js
@@ -1,31 +1,23 @@
-var fs = require('fs')
-var http = require('http')
-var assets = require('./assets.js')
+// var app = require('express')()  otra manera de declarar
+var express = require('express')
+var app = express()
 
-var server = http.createServer( (req,res) => {
-	switch(req.url) {
-		case '/':
-			assets.serveStatic('index.html', (err, content) => {
-				res.end(content)
-			})
-			break
-		case '/app.js':
-			assets.serveStatic('app.js', (err, content) => {
-				res.end(content)
-			})
-			break
-		case '/app.css':
-			assets.serveStatic('app.css', (err, content) => {
-				res.end(content)
-			})
-			break
-		default:
-			res.statusCode = 404
-			res.end('Not Found')
-			break
-	}
+/*app use sirve para registrar middlewares, que son funciones que se ejecutan que se ejecutan de 
+forma secuencial cada vez que llega un request */
+
+/*establece directorio donde estaran los archivos static*/
+app.use(express.static('public')) /* express.static('public') es un middleware y evalua si el request que llega esta pidiendo un static file y una vez haga eso va a cortar la secuencia y no ejecuta el resto de endPoints*/ 
+
+// GET /votes
+app.get('/votes', function (req, res) {
+	res.json([])
 })
 
-server.listen(3000, () => {
-	console.log('hola ya corriendo')
+// POST /vote/123
+app.post('/vote/:id', function (req, res) { /*express lo procesa como un parametro*/
+
+})
+
+app.listen(3000, function () {
+	console.log('hola ya corriendo con Express')
 })
