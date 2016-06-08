bf69dc96c75403c3f185666687044377ee1cd7c8
Daniel H <frontend@reddesignsystems.com>
2016-06-08 17:23:22 -0500

    Build API for votes show

--- src/client/render/index.js
+++ src/client/render/index.js
@@ -10,7 +10,7 @@ let template = `<article class="tv-show">
 					<div class="right info">
 						<h1>:name:</h1>
 						<p>:summary:</p>
-						<button class="like">💙</button>
+						<button data-id=:id: class="like">💙</button>
 					</div>
 				</article>`
 
@@ -24,6 +24,7 @@ export default function renderShows(shows) {
 			.replace(':img:', show.image ? show.image.medium : '')
 			.replace(':summary:', show.summary)
 			.replace(':img alt:', show.name + "Logo")
+			.replace(':id:', show.id)
 		// Convirtiendo en jquery object
 		var $article = $(article)
 		$tvShowsContainer.append($article)
--- src/client/tv-shows-container/index.js
+++ src/client/tv-shows-container/index.js
@@ -4,11 +4,15 @@ var $tvShowsContainer = $('#app-body').find('.tv-shows');
 
 $tvShowsContainer.on('click', 'button.like', function (ev) {
 	var $this = $(this);
-	$this.animate({
-		'fontSize': '30px'
-	}, 'fast')
-	$this.closest('.tv-show')  /*closets function de jquery que busca padre que cumpla condicion*/
-		.toggleClass('liked')
+	var id = $this.data('id') // data-id button
+	$.post('/vote/' + id, function () {
+		
+		$this.animate({
+			'fontSize': '30px'
+		}, 'fast')
+		$this.closest('.tv-show')  /*closets function de jquery que busca padre que cumpla condicion*/
+			.toggleClass('liked')
+	})
 })
 
 export default $tvShowsContainer
