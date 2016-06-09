5b021f284e622cbacb54caaead161a8cfb62a29f
Daniel H <frontend@reddesignsystems.com>
2016-06-09 17:38:34 -0500

    Modularize routes with express Router - add suffix /api to /votes

---
+++ src/server/api/index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+const router = express.Router()  
+
+const votes = {}
+
+
+// GET /api/votes
+router.get('/votes', (req, res) => {
+	res.json(votes)
+})
+
+// POST /api/vote/123
+router.post('/vote/:id', (req, res) => { /*express lo procesa como un parametro*/
+	let id = req.params.id
+	if (votes[id] === undefined)
+		votes[id] = 1
+	else
+		votes[id] = votes[id] + 1
+
+	res.json({ votes: votes[id]})
+})
+
+export default router
--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@
 import express from 'express'
 const app = express()
 
-const votes = {}
+import api from '../../src/server/api'
 
 /*app use sirve para registrar middlewares, que son funciones que se ejecutan que se ejecutan de 
 forma secuencial cada vez que llega un request */
@@ -10,20 +10,7 @@ forma secuencial cada vez que llega un request */
 /*establece directorio donde estaran los archivos static*/
 app.use(express.static('public')) /* express.static('public') es un middleware y evalua si el request que llega esta pidiendo un static file y una vez haga eso va a cortar la secuencia y no ejecuta el resto de endPoints*/ 
 
-// GET /votes
-app.get('/votes', (req, res) => {
-	res.json(votes)
-})
-
-// POST /vote/123
-app.post('/vote/:id', (req, res) => { /*express lo procesa como un parametro*/
-	let id = req.params.id
-	if (votes[id] === undefined)
-		votes[id] = 1
-	else
-		votes[id] = votes[id] + 1
-
-	res.json({ votes: votes[id]})
-})
+/*montar modulo api en /api*/
+app.use('/api', api)
 
 app.listen(3000, () => console.log('hola ya corriendo con Express'))
