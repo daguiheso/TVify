17e80e315ebe225a68c0e1af01bec86b7699f174
Daniel H <frontend@reddesignsystems.com>
2016-06-10 16:02:19 -0500

    Scheme mongosee and implemented Vote model to get&post api/votes

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,24 +1,49 @@
 import express from 'express'
 const router = express.Router()  
 
-const votes = {}
+import Vote from '../models' 
+import mongoose from 'mongoose'
+mongoose.connect('mongodb://localhost/tvify')
 
 
 // GET /api/votes
 router.get('/votes', (req, res) => {
 	console.log('Estoy en GET /votes')
-	res.json(votes)
+	Vote.find({}, (err, docs) => {
+		if(err) {
+			return res.sendStatus(500).json(err)
+		}
+		res.json(docs)
+	})
 })
 
 // POST /api/vote/123
 router.post('/vote/:id', (req, res) => { /*express lo procesa como un parametro*/
 	let id = req.params.id
-	if (votes[id] === undefined)
-		votes[id] = 1
-	else
-		votes[id] = votes[id] + 1
 
-	res.json({ votes: votes[id]})
+	var onSave = function (vote) {
+		return err => {
+			if (err) {
+				return res.sendStatus(500).json(err)
+			}
+			res.json(vote)
+		}
+	}
+
+	/*primer documento que venga con esta condicion*/
+	Vote.findOne({ showId: id}, (err, doc) => {
+		if(doc) {
+			/*actualizo este doc*/
+			doc.count = doc.count + 1
+			doc.save(onSave(doc))
+		} else {
+			/*creo un doc nuevo y le pongo count 1*/
+			let vote = new Vote()
+			vote.showId = id
+			vote.count = 1
+			vote.save(onSave(vote))
+		}
+	})    
 })
 
 export default router
---
+++ src/server/models/index.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+/*Vote
+	showId
+	count
+*/
+
+/*Definiendo esquema*/
+let VoteSchema = new mongoose.Schema({
+	/*columnas*/
+	showId: { type: Number, required: true, unique: true },  /*type es el unico obligatorio, y recibe todos los tipos de js y los de mongoose com MixedType para datos mixtos*/
+	count: { type: Number, default: 0}
+})
+
+/*Registrando variable en mongoose*/
+export default mongoose.model('Vote', VoteSchema) /*por parametro recibe nombre del modelo (Vote), y esquema definido*/
